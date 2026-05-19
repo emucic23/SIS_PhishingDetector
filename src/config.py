211ab768243a3c7e9ef7820747b570f4b33d1dad
emucic23 <emucic23@student.foi.hr>
2026-05-19 12:30:03 +0200

    Dodana mailpit konfiguracija i docker setup

---
+++ src/config.py
@@ -0,0 +1,34 @@
+
+BODOVI_SUMNJIVI_URL = 3
+BODOVI_LAZNI_POSILJATELJ = 2
+BODOVI_REPLY_TO = 2
+BODOVI_ANOMALIJE_JEZIKA = 3
+
+
+PRAG_PHISHING = 7
+PRAG_SUMNJIVO = 4
+
+MAILPIT_HOST = "mailpit"
+MAILPIT_PORT = 1025
+MAILPIT_API = "http://mailpit:8025/api"
+
+
+PUTANJA_DATASET = "data/dataset.csv"
+PUTANJA_REZULTATI = "results/results.csv"
+
+
+POZNATE_DOMENE = [
+    "gmail.com",
+    "outlook.com",
+    "yahoo.com",
+    "fer.hr",
+    "foi.hr",
+    "unizg.hr"
+]
+
+
+URGENTNE_RIJECI = [
+    "urgent", "immediately", "suspended", "verify",
+    "click here", "limited time", "winner", "congratulations",
+    "account blocked", "unusual activity", "confirm now"
+]
